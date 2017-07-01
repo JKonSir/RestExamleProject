d0bbec563b8d87825e4bdb0a27a7fa33b6bdf7b8
konstantins304 <konstantins304@gmail.com>
2017-07-01 17:50:48 +0400

    Initial commit

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"app"
+)
+
+func main() {
+	a := app.App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	a.Run(":8080")
+}
